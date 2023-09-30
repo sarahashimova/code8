1dcabab93f8ce522403ab629faa5cfc1054f5613
sarahashimova <sara.alyva@gmail.com>
2023-09-30 16:14:13 +0400

    table

--- src/components/users/index.jsx
+++ src/components/users/index.jsx
@@ -7,32 +7,52 @@ function User() {
       <table className="user-table">
         <thead>
           <tr className="table-header">
-            <th>Name Surname</th>
-            <th>Occupation</th>
-            <th>Mobile Number</th>
-            <th>Date 1</th>
-            <th>Date 2</th>
+            <th>Ad Soyad</th>
+            <th>Tədris Sahəsi</th>
+            <th>Telefon nömrəsi</th>
+            <th>Müraciət tarixi</th>
+            <th>Görüş tarixi</th>
             <th>Status</th>
-            <th>...</th>
+            <th>Detallı</th>
           </tr>
         </thead>
         <tbody>
           <tr className="table-row">
-            <td>John Doe</td>
-            <td>Engineer</td>
+            <td>Rabiyya Huseynzade</td>
+            <td>Front-End</td>
             <td>123-456-7890</td>
             <td>2023-09-30</td>
             <td>2023-10-01</td>
-            <td>
-              <div className="status">
-                <span className="status-circle red"></span>
-                <span className="status-circle blue"></span>
-                <span className="status-circle green"></span>
-              </div>
-            </td>
+            <td><div className='status'>Aktiv</div></td>
             <td>...</td>
           </tr>
-          {/* Add more rows with user data here */}
+          <tr className="table-row">
+            <td>Rabiyya Huseynzade</td>
+            <td>Front-End</td>
+            <td>123-456-7890</td>
+            <td>2023-09-30</td>
+            <td>2023-10-01</td>
+            <td>Aktiv</td>
+            <td>...</td>
+          </tr>
+          <tr className="table-row">
+            <td>Rabiyya Huseynzade</td>
+            <td>Front-End</td>
+            <td>123-456-7890</td>
+            <td>2023-09-30</td>
+            <td>2023-10-01</td>
+            <td>Aktiv</td>
+            <td>...</td>
+          </tr>
+          <tr className="table-row">
+            <td>Rabiyya Huseynzade</td>
+            <td>Front-End</td>
+            <td>123-456-7890</td>
+            <td>2023-09-30</td>
+            <td>2023-10-01</td>
+            <td>Aktiv</td>
+            <td>...</td>
+          </tr>
         </tbody>
       </table>
     </div>
